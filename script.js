6ff861879a9f951c4fadeb9dd12e9af0c277e5cd
Myroslav <myroslavtp@gmail.com>
2023-07-18 10:55:21 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+function getUniqueSymbol(text) {
+    let words = [];
+    let uniqueLetters = [];
+
+    text = text.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ");
+    words = text.toLowerCase().split(' ');
+
+    words.forEach((item) => {
+        let uniq = '';
+        for (let i = 0; i < item.length; i++) {
+            if (uniq.includes(item[i])) {
+                uniq = uniq.replace(item[i], '')
+            } else if (!uniq.includes(item[i])) {
+                uniq += item[i]
+            }
+        }
+        uniqueLetters.push(uniq[0]);
+    });
+    
+    let uniqueChars = [...new Set(uniqueLetters)];
+
+    var intersections = uniqueChars.filter(e => uniqueLetters.indexOf(e) !== -1);
+
+    return intersections;
+}
+
+
+let testText = `The Tao gave birth to machine language.
+Machine language gave birth to the assembler.
+The assembler gave birth to the compiler.
+Now there are ten thousand languages.
+Each language has its purpose, however humble.
+Each language expresses the Yin and Yang of software. 
+Each language has its place within the Tao.
+But do not program in COBOL if you can avoid it.
+-- Geoffrey James, "The Tao of Programming"`;
+
+console.log(getUniqueSymbol(testText));
+
+
+
